f184b867ae79c8ca9b8940ac4a7fb37775bef0a8
pg25wyatt <pg25wyatt@vfs.com>
2024-04-09 14:42:51 -0700

    added hierarchy hide script

---
+++ Runtime/HideHierarchy.cs
@@ -0,0 +1,39 @@
+using PlasticGui.WorkspaceWindow.QueryViews.Changesets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityWorkflowTools
+{
+#if UNITY_EDITOR
+    [ExecuteAlways]
+    public class HideHierarchy : MonoBehaviour
+    {
+        [SerializeField] private bool _hidden = true;
+
+        private void OnValidate()
+        {
+            HideChildren(_hidden);
+        }
+
+        private void Awake()
+        {
+            HideChildren(_hidden);
+        }
+
+        private void HideChildren(bool hidden)
+        {
+            Transform[] children = GetComponentsInChildren<Transform>();
+
+            foreach (Transform child in children)
+            {
+                if (child.gameObject == gameObject) continue;
+
+                HideFlags flags = HideFlags.HideInInspector | HideFlags.HideInHierarchy;
+                if (!hidden) flags = HideFlags.None;
+                child.gameObject.hideFlags = flags;
+            }
+        }
+#endif
+    }
+}
